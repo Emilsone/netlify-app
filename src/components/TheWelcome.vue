d5f8aca319c78c154c062a74f2f29de2b9de8726
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-14 15:11:47 +0100

    restructure codebase

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,29 +1,55 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-    <form name="contact" method="POST" netlify>
-      <p><input type="hidden" name="form-name" value="Contact" /></p>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </main>
-</template>
+  <form>
+    <p>
+      <label>
+        Your Name: <input type="text" name="name" v-model="form.name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email: <input type="email" name="email" v-model="form.email" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Message: <textarea name="message" v-model="form.message" />
+      </label>
+    </p>
+    <p>
+      <button type="submit" @click.prevent="handleSubmit">Send</button>
+    </p>
+  </form>
+  </template>
+  
+  
+  <script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          email: '',
+          message: '',
+        },
+      };
+    },
+    methods: {
+      encode(data) {
+        return Object.keys(data)
+          .map(
+            key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+          )
+          .join('&');
+      },
+      handleSubmit() {
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: this.encode({ 'form-name': 'contact', ...this.form }),
+        })
+          .then(() => alert('Success!'))
+          .catch(error => alert(error));
+      },
+    },
+  };
+  </script>
